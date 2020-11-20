9d6dd0dbc2c6d199a4ffd281743bfd9a30b4f869
Brooke McBain <mcbainbrooke@gmail.com>
2020-11-20 09:23:26 -0600

    Geocode API is up and running now with map view and pointer to neighbors' locations

---
+++ Controllers/EmailController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Capstone.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Capstone.Controllers
+{
+    public class EmailController : Controller
+    {
+        public EmailController()
+        {
+
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> SendMail(Email email)
+        {
+            return View(); 
+        }
+    }
+}
---
+++ Services/IEmailService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Capstone.Services
+{
+    //public class SendMail
+    //{
+
+    //    string mailkitkey = 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; 
+    //}
+}
